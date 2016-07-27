65c75716c0239ec535c688070f79d9fbd1c5714f
Ryan Soklaski <rsoklaski@gmail.com>
2016-07-27 00:06:44 -0400

    Added decorator base

---
+++ custom_inherit/decorator_base.py
@@ -0,0 +1,32 @@
+from functools import wraps
+
+
+class DocInheritDecorator(object):
+    def __init__(self, prnt_doc):
+        self.prnt_doc = prnt_doc if isinstance(prnt_doc, str) else prnt_doc.__doc__
+
+    def __call__(self, func):
+        @wraps(func)
+        def wrapped(*args, **kwargs):
+            return func(*args, **kwargs)
+        wrapped.__doc__ = self.doc_merger(self.prnt_doc, func.__doc__)
+
+    @staticmethod
+    def doc_merger(prnt_attr_doc, child_doc):
+        """ Merge the docstrings of method or property from parent class and the corresponding
+            attribute of its child.
+
+            Parameters
+            ----------
+            prnt_cls_doc: Union[None, str]
+            child_doc: Union[None, str]
+
+            Raises
+            ------
+            NotImplementedError
+
+            Notes
+            -----
+            This works for properties, methods, static methods, class methods, and
+            decorated methods/properties."""
+        raise NotImplementedError
