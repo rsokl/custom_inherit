e7ecabe33cd982b0b08796ec7b370722276fb383
Ryan Soklaski <rsoklaski@gmail.com>
2017-01-07 17:24:54 -0500

    Added tests. _Store getter now catches ValueError.

---
+++ tests/store_test.py
@@ -0,0 +1,60 @@
+""" Tests behavior of custom_inherit._Store """
+
+from custom_inherit import _Store, store, style_store
+
+import pytest
+
+def bad_style_sig1(x, y, z): return None
+
+def bad_style_sig2(x): return None
+
+def bad_style_type(x, y): return float(x)
+
+def bad_style_type2(x, y): return 1
+
+def good_style1(x, y): return ",".join((x, y))
+
+def good_style2(x, y): return None
+
+
+def test_Store():
+    _store = _Store()
+    with pytest.raises(TypeError):
+        _store[bad_style_sig1]
+
+    with pytest.raises(TypeError):
+        _store["bad_style"] = bad_style_sig1
+
+    with pytest.raises(TypeError):
+        _store[bad_style_sig2]
+
+    with pytest.raises(TypeError):
+        _store[bad_style_type]
+
+    with pytest.raises(TypeError):
+        _store[bad_style_type2]
+
+    assert _store[good_style1] == good_style1
+    assert _store[good_style2] == good_style2
+
+    _store["test_style"] = good_style1
+    assert _store["test_style"] == good_style1
+
+    _store = _Store(test_style=good_style1)
+    assert _store._store == dict(test_style=good_style1)
+
+    assert _store.pop("test_style") == good_style1
+    assert _store._store == dict()
+
+
+def test_store():
+    assert isinstance(store, _Store)
+    assert set(store.items()) == set((key, getattr(style_store, key)) for key in style_store.__all__)
+    assert "parent" in store.keys()
+    assert "numpy" in store.keys()
+
+
+
+
+
+
